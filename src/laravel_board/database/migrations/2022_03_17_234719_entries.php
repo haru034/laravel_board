36900a48d035000d291ec5ee3e4603f3ee20b91b
haru034 <haru034@icloud.com>
2022-03-18 00:55:46 +0900

    #3 add_entriesテーブルのマイグレーションファイル

---
+++ src/laravel_board/database/migrations/2022_03_17_234719_entries.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Entries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id'); // usersテーブルのid
+            $table->foreign('user_id') 
+                ->references('id')->on('users')
+                ->onDelete('cascade');  // usersテーブルのidが削除された場合、同じuser_idをentriesテーブルから削除
+            $table->string('thread_title'); // スレッドのタイトル
+            $table->string('thread_text'); // スレッドの本文
+            $table->timestamps(); // 作成日時と更新日時
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+}
